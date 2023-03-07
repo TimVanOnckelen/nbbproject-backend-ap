54a658705209964fd4c105ecaf02d8ec0239fb88
Brayan de Krom <brayan.dekromhernandez@ap.be>
2023-03-07 20:21:59 +0100

    Refactor the repository

--- NBB-Project-Back-Enc/NBB.Api/Repositories/Repository.cs
+++ NBB-Project-Back-Enc/NBB.Api/Repositories/Repository.cs
@@ -1,6 +1,47 @@
-﻿namespace NBB.Api.Repository
+﻿using NBB.Api.Models;
+using NBB.Api.Repository;
+using System.Diagnostics.Contracts;
+using System.Linq.Expressions;
+
+
+namespace Repositories
 {
-    public class Repository
+    public interface IRepository
     {
+        void Add(Onderneming onderneming);
+        void Delete(Onderneming onderneming);
+        Onderneming Get(string ondernemingsnummer);
+        IEnumerable<Onderneming> GetAll();
+        void Update(Onderneming onderneming);
+    }
+
+    public class Repository : IRepository
+    {
+        private readonly List<Onderneming> _ondernemingen;
+        public Repository()
+        {
+            _ondernemingen = new List<Onderneming>();
+        }
+        public IEnumerable<Onderneming> GetAll()
+        {
+            return _ondernemingen;
+        }
+        public Onderneming Get(string ondernemingsnummer)
+        {
+            return _ondernemingen.FirstOrDefault(x => x.OndernemingsNummer == ondernemingsnummer);
+        }
+        public void Add(Onderneming onderneming)
+        {
+            _ondernemingen.Add(onderneming);
+        }
+        public void Delete(Onderneming onderneming)
+        {
+            _ondernemingen.Remove(onderneming);
+        }
+        public void Update(Onderneming onderneming)
+        {
+            var toUpdate = Get(onderneming.OndernemingsNummer);
+            toUpdate = onderneming;
+        }
     }
 }
