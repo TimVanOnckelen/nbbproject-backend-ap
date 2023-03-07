199d6e7ddcb27e51482520e73e6e32ac874ef451
Brayan de Krom <brayan.dekromhernandez@ap.be>
2023-03-07 19:10:18 +0100

    Create Models, startup and Contoller files

---
+++ NBB-Project-Back-Enc/NBB.Api/Models/Adres.cs
@@ -0,0 +1,6 @@
+﻿namespace NBB.Api.Models
+{
+    public class Adres
+    {
+    }
+}
--- NBB-Project-Back-Enc/NBB.Api/Startup.cs
+++ NBB-Project-Back-Enc/NBB.Api/Startup.cs
@@ -1,6 +1,12 @@
-﻿namespace NBB.Api
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace NBB.Api
 {
     public class Startup
     {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
     }
 }
