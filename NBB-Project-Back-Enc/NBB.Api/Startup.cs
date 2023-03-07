7eefaa8f2e361d5f0b775eb7703ecadf07469958
Eli Winderickx <eli.winderickx@student.ap.be>
2023-03-07 19:16:37 +0100

    Eerste Push MVC Model. Not Working

--- NBB-Project-Back-Enc/NBB.Api/Startup.cs
+++ NBB-Project-Back-Enc/NBB.Api/Startup.cs
@@ -2,5 +2,46 @@
 {
     public class Startup
     {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //services.AddScoped<IContractRepository, EfContractRepository>();
+            services.AddControllers();
+            services.AddSwaggerGen();
+            var connection = configuration.GetConnectionString("NBBDatabase");
+            //services.AddDbContext<ContractDBContext>(x => x.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("./swagger/v1/swagger.json", "NBB API");
+                    c.RoutePrefix = String.Empty;
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler(new ExceptionHandlerOptions
+                {
+                    ExceptionHandler = context => context.Response.WriteAsync("OOPS")
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
     }
 }
---
+++ NBB-Project-Back-Enc/NBB.Api/services/IDatasource.cs
@@ -0,0 +1,12 @@
+﻿
+namespace NBB.Api.Services
+{
+    public interface IDatasource
+    {
+            IEnumerable<Contract> GetAll();
+            Contract Get(int id);
+            void Add(Contract contract);
+            void Delete(Contract contract);
+            void Update(Contract contract);
+    }
+}
