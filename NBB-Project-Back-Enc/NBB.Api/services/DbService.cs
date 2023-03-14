9a6b6264fd990a2e5571b21552203cfbb0a4a160
Brdekrom <brdekrom@gmail.com>
2023-03-14 18:17:23 +0100

    Adding DBService

--- NBB-Project-Back-Enc/NBB.Api/services/DbService.cs
+++ NBB-Project-Back-Enc/NBB.Api/services/DbService.cs
@@ -1,6 +1,50 @@
-﻿namespace NBB.Api.services
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NBB.Api.services
 {
-    public class DbService
+    public class DbService<T> : IDbService<T> where T : class
     {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public DbService(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(int id, T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            T entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
     }
+
 }
