83cf2f78810e9923aa8e13264b403fc9f0f395c3
Eli Winderickx <eli.winderickx@student.ap.be>
2023-03-21 21:09:58 +0100

    Auto stash before rebase of "main"

---
+++ NBB-Project-Back-Enc/NBB.Api/Repositories/MySQLEnterprise.cs
@@ -0,0 +1,45 @@
+﻿using NBB.Api.Models;
+using NBB.Api.services;
+
+namespace NBB.Api.Repositories
+{
+    public class MySQLEnterprise : IRepository
+    {
+        private EnterpriseDbContext _context;
+
+        public MySQLEnterprise(EnterpriseDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Enterprise onderneming)
+        {
+            _context.Enterprise.Add(onderneming);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Enterprise onderneming)
+        {
+            var toDelete = Get(onderneming.EnterpriseNumber);
+            _context.Enterprise.Remove(toDelete);
+            _context.SaveChanges();
+        }
+
+        public Enterprise Get(string ondernemingsnummer)
+        {
+            return _context.Enterprise.FirstOrDefault(x => x.EnterpriseNumber == ondernemingsnummer);
+        }
+
+        public IEnumerable<Enterprise> GetAll()
+        {
+            return _context.Enterprise;
+        }
+
+        public void Update(Enterprise onderneming)
+        {
+            var toUpdate = Get(onderneming.EnterpriseNumber);
+            toUpdate.EnterpriseName = onderneming.EnterpriseName;
+            toUpdate.AccountingDataURL = onderneming.AccountingDataURL;
+        }
+    }
+}
---
+++ NBB-Project-Back-Enc/NBB.Api/services/EnterpriseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NBB.Api.Models;
+
+namespace NBB.Api.services
+{
+    public class EnterpriseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "Enterprise");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Enterprise> Enterprise { get; set; }
+        public DbSet<Address> Address { get; set; }
+    }
+}
