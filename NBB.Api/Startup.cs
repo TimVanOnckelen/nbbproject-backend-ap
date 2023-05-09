77bb33f3268ee7c2665a0d796dd5da0c4f6438f1
Eli Winderickx <eli@winderickx.me>
2023-05-09 19:18:17 +0200

    IT WORKS!!!

--- NBB.Api/Startup.cs
+++ NBB.Api/Startup.cs
@@ -36,7 +36,26 @@ namespace NBB.Api
             services.AddScoped<IEnterpriseRepository, EfNBBRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddControllers();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c=>
+            {
+                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
+                            },
+                        new string[] {}
+                    }
+                });
+            });
             var connection = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
             services.AddDbContext<NBBDBContext>(x => x.UseSqlServer(connection));
         }
